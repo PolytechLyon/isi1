a8dfcc5900598d2ec32b4cc0c265d3f2936b48ec
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2024-10-27 16:11:07 +0100

    Slides for introduction and JS

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import Reveal from 'reveal.js';
+import RevealMarkdown from 'reveal.js/plugin/markdown/markdown';
+import RevealHighlight from 'reveal.js/plugin/highlight/highlight';
+import RevealNotes from 'reveal.js/plugin/notes/notes';
+
+Reveal.initialize({
+    hash: true,
+
+    plugins: [ RevealMarkdown, RevealHighlight, RevealNotes ]
+});
