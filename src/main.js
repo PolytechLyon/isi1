a88b0bc88a2529fb16a0471699352950d719bd93
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2025-03-05 19:10:50 +0100

    Add evaluate button to snippets

--- src/main.js
+++ src/main.js
@@ -3,6 +3,23 @@ import RevealMarkdown from 'reveal.js/plugin/markdown/markdown';
 import RevealHighlight from 'reveal.js/plugin/highlight/highlight';
 import RevealNotes from 'reveal.js/plugin/notes/notes';
 
+const evalConsole = {
+    element: undefined,
+    timeoutId: undefined,
+    close() {
+        evalConsole.element?.close();
+        evalConsole.element.innerHTML = "";
+        document.removeEventListener("keyup", evalConsole.close);
+    },
+    open(html) {
+        evalConsole.element.innerHTML = html;
+        evalConsole.element?.showModal();
+        document.addEventListener("keyup", evalConsole.close);
+        clearTimeout(evalConsole.timeoutId);
+        evalConsole.timeoutId = setTimeout(evalConsole.close, 7_000);
+    }
+};
+
 function createDemoFrame(element) {
     const example = element.getAttribute('data-code-example');
     if (!example) return;
@@ -31,32 +48,73 @@ function createDemoFrame(element) {
     `;
 }
 
+function extractTextFromEvent(event) {
+    return event.target.closest('code')?.innerText?.trim();
+}
+
 function copyToClipboard(event) {
-    const text = event.target.closest('code')?.innerText?.trim()
-    if (text) {
-        navigator.clipboard.writeText(text).then();
+    const text = extractTextFromEvent(event);
+    if (!text) {
+        return;
     }
+    navigator.clipboard.writeText(text).then();
+}
+
+function evaluateSnippet(event) {
+    const text = extractTextFromEvent(event);
+    if (!evalConsole.element) {
+        return;
+    }
+    const output = [];
+    let value;
+    const originalLog = console.log;
+    try {
+        console.log = (...args) => output.push(args);
+        value = eval(text);
+    } catch (error) {
+        value = error;
+    } finally {
+        console.log = originalLog;
+    }
+    if (value !== undefined) {
+        output.push([], [value]);
+    }
+    const html = output.map(args => args.join(" ")).join("<br />");
+    evalConsole.open(html);
 }
 
 function createCodeIcons(element) {
     const externalExample = element
         .parentElement
         .previousElementSibling?.getAttribute('data-external-example');
+    const isSnippet = !!element.closest(".javascript");
     const externalLink = externalExample ? `<a
                 href='${externalExample}'
                 target="_blank">
                 <img
                     src="images/view.svg"
                     alt="Exemple extèrne" />
-          </a>` : '';
+        </a>` : '';
+    const evalButton = isSnippet ? `<a
+                class="eval-icon no-print">
+                <img src="images/play.svg" alt="Évaluer" />
+        </a>` : '';
+
     element.innerHTML += `<div class="icons">
-            <a 
-                class="no-print">
+            <a
+                class="copy-icon no-print">
                 <img src="images/copy.svg" alt="Copier" />
             </a>
             ${externalLink}
+            ${evalButton}
         </div>`;
-    element.getElementsByTagName("a")[0].addEventListener("click", copyToClipboard);
+    element.getElementsByClassName("copy-icon")[0].addEventListener("click", copyToClipboard);
+    element.getElementsByClassName("eval-icon")[0]?.addEventListener("click", evaluateSnippet);
+}
+
+function prepareEvalConsole() {
+    evalConsole.element = document.getElementById("eval-console");
+    evalConsole.element.addEventListener("click", evalConsole.close);
 }
 
 Reveal.initialize({
@@ -64,6 +122,7 @@ Reveal.initialize({
     plugins: [ RevealMarkdown, RevealHighlight, RevealNotes ],
     navigationMode: 'linear',
 }).then(() => {
+    prepareEvalConsole();
     document.querySelectorAll('.reveal section [data-code-example]').forEach(createDemoFrame);
     document.querySelectorAll('.reveal section pre code').forEach(createCodeIcons);
     Reveal.sync();
