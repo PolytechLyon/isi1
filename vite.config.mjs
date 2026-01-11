e0ac004e25e8366741bb1da3b0fc99abec382f9f
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2026-01-11 16:22:09 +0100

    Undo substitute isi1 with isi
    This reverts commit 1c1f1220228f515860c71221f65218da687c5d6f.

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: process.env.CI ? '/isi/' : undefined,
+    base: process.env.CI ? '/isi1/' : undefined,
     build: {
         chunkSizeWarningLimit: 2048,
     },
